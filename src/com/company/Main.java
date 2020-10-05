036b1c10019dc4ca56b3b76a274c1fac5d913f13
CyberSander <cyber.sander777@gmail.com>
2020-10-05 11:22:56 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,8 +14,8 @@ public class Main {
         public Transition(String s) {
             String[] in = s.split(" ");
             this.from = Integer.parseInt(in[0].trim());
-            this.to = Integer.parseInt(in[1].trim());
-            this.a = in[2].trim().toCharArray()[0];
+            this.to = Integer.parseInt(in[2].trim());
+            this.a = in[1].trim().toCharArray()[0];
         }
     }
 
@@ -23,15 +23,14 @@ public class Main {
         public HashSet<Character> A;
         public HashSet<Integer> S;
         public int s_0;
-        public HashSet<Transition> F;
+        public HashSet<Integer> F;
+        public HashSet<Transition> T;
 
         public StateMachine(String in) {
             this.A = new HashSet<>();
             String[] inputs = in.split("\n");
-            for (int i = 0; i < 255; i++) {
-                if (String.valueOf((char) i).matches(inputs[0].trim())) {
-                    this.A.add((char) i);
-                }
+            for (int i = 0; i < Integer.parseInt(inputs[0].trim()); i++) {
+               this.A.add((char)(97 + i));
             }
             this.S = new HashSet<>();
             for (int i = 0; i <= Integer.parseInt(inputs[1].trim()); i++) {
@@ -39,8 +38,13 @@ public class Main {
             }
             this.s_0 = Integer.parseInt(inputs[2].trim());
             this.F = new HashSet<>();
-            for (int i = 3; i < inputs.length; i++) {
-                this.F.add(new Transition(inputs[i]));
+            String[] fStates = inputs[3].split(" ");
+            for(int i = 1; i < fStates.length; i++) {
+                this.F.add(i);
+            }
+            this.T = new HashSet<>();
+            for (int i = 5; i < inputs.length; i++) {
+                this.T.add(new Transition(inputs[i]));
             }
         }
 
@@ -50,7 +54,7 @@ public class Main {
             while (true) {
                 int lengthBefore = reachableStates.size();
 
-                for (Transition t : this.F) {
+                for (Transition t : this.T) {
                     if (reachableStates.contains(t.from)) {
                         reachableStates.add(t.to);
                     }
@@ -71,7 +75,7 @@ public class Main {
 
         public HashSet<Integer> getDeadEnds() {
             HashSet<Integer> reachable = this.getReachableStates();
-            for (Transition t : this.F) {
+            for (Transition t : this.T) {
                 reachable.remove(t.from);
             }
             return reachable;
